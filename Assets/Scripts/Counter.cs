11bfba816a45c521e9c0651c455ef18b22a45fd2
phip0n <107852454+phip0n@users.noreply.github.com>
2024-06-23 16:30:27 +0300

    main

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -6,18 +6,18 @@ public class Counter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
     [SerializeField] private float _delay;
-    [SerializeField] private float _increase;
+    [SerializeField] private float _addedValue;
     [SerializeField] private float _currentValue;
 
     private bool _isActive = false;
     private Coroutine _count;
 
-    void Start()
+    private void Start()
     {
         _currentValue = 0;
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
@@ -30,7 +30,11 @@ public class Counter : MonoBehaviour
         if (_isActive)
         {
             _isActive = false;
-            StopCoroutine(_count);
+
+            if (_count != null)
+            {
+                StopCoroutine(_count);
+            }
         }
         else
         {
@@ -42,13 +46,12 @@ public class Counter : MonoBehaviour
     private IEnumerator Count()
     {
         var wait = new WaitForSeconds(_delay);
-        yield return wait;
 
         while (_isActive)
         {
-            _currentValue += _increase;
-            _text.text = _currentValue.ToString();
             yield return wait;
+            _currentValue += _addedValue;
+            _text.text = _currentValue.ToString();
         }
     }
 }
